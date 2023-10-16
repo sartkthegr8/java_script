2189508c42ef1d5ed827428767d8e5ef0685c6cb
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-10-16 18:21:09 +0000

    added string

---
+++ basics_js_1/4_strings.js
@@ -0,0 +1,22 @@
+const name ="sartk"
+const count=50
+
+// console.log(name + count) //old syntax
+
+// console.log('hello ${name} and my no. is ${count}'); //comma    'c'
+// console.log(`hello ${name} and my no. is ${count}`); //backtick `c`
+
+const gname = new String('sartk007')
+// console.log(typeof gname); //object
+
+// console.log(gname);
+
+// console.log(gname[0]); //s
+
+// console.log(gname.length);
+
+// console.log(gname.toUpperCase()); //original value not change
+
+// console.log(gname.charAt(2));
+
+// console.log(gname.indexOf('t'));
