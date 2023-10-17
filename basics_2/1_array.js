dc4fbf6ec0b6a153e26e7d1efaac873eb893ff3e
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-10-17 09:14:32 +0000

    done

--- basics_2/1_array.js
+++ basics_2/1_array.js
@@ -1,10 +1,10 @@
 // const Arr =[0,1,2,3,4,5,true,"sarthak"]
 
-const Arr1 =[1,2,3,4,5]
+// const Arr1 =[1,2,3,4,5]
 
 // const arr1 = new Array(1,2,3,4,5)
 
-// Shallow copy (same refernece) & deep copy (diff reference)
+//  ❤️Shallow copy (same refernece) & ❤️deep copy (diff reference)
 
 
 // console.log(Arr1);
@@ -26,6 +26,45 @@ const Arr1 =[1,2,3,4,5]
 // [not chnge on orignal array]
 // [start range include]
 
+//**********push / conacet / spread /flat ***********/
+
+// const a =[1,2,3]
+// const b =['x','y','z']
+
+// push ❤️
+// a.push(b) //push dont merger element ,it push whole array as element into another 
+// console.log(a);
+
+// concat ❤️
+// const c=a.concat(b) //concate return a new array with both array elements 
+// console.log(c);
+
+//spread '...' operator ❤️
+// const d = [...a,...b] // return new array as manny possiple array elements
+// console.log(d);
+
+//flat ❤️
+
+// const ar=[1,2,[3,4,['x','c','v','h',[21,32,65,45,8],'b','n'],'s','d'],55,65] //contain 3-level of subarray
+// //  const ans = ar.flat(1) //merge 1 level subarray
+// const ans = ar.flat(3) //merge 3 level subarray
+// const ans = ar.flat(infinity) //merge to all level subarray
+// console.log(ans);
+
+//from ❤️
+// console.log(Array.from("sarthak")); //make character of given string
+
+// 🙅‍♂️console.log(Array.from({name:"sarthak"})); //make character of key : value >>not possible here no kew define so gave >> [] 'empty object'
+
+
+//of ❤️
+
+// let a=1;
+// let b=2;
+// let c=3;
+// let d=Array.of(a,b,c); //merge all variable into array
+
+// console.log(d);
 
 
 
