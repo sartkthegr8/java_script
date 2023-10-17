d025fbd9bac2967e2f4f932210b96154f43c6957
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-10-17 07:07:37 +0000

    date done

---
+++ basics_js_1/5_date.js
@@ -0,0 +1,31 @@
+// console.log("heelo");
+
+// console.log(Date()) //Tue Oct 17 2023 06:41:15 GMT+0000 (Coordinated Universal Time)
+
+// let myDate = new Date()
+// console.log(myDate);  //2023-10-17T06:41:54.608Z
+
+// console.log(myDate.toString());  //Tue Oct 17 2023 06:41:15 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString()); //Tue Oct 17 2023
+
+// console.log(myDate.toISOString()); //2023-10-17T06:40:28.880Z
+
+// console.log(myDate.toJSON()); //2023-10-17T06:40:28.880Z
+
+// console.log(myDate.toLocaleDateString()); //  10/17/2023
+
+// console.log(myDate.toLocaleString()); //  10/17/2023, 6:43:32 AM
+
+//create date
+// let myCreateDate = new Date(2023,0,23) // month start with >>0[jan]->11[dec]
+// console.log(myCreateDate.toDateString());
+
+// let myCreateDate = new Date("2023-00-23") // 00-->>error with month
+// let myCreateDate = new Date("2023-01-23") // 00-->>error with month // 01-> [jan] ..here
+// console.log(myCreateDate.toLocaleString());
+
+// console.log(Date.now()); //time stamp in milli-second
+
+
+
