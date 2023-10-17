396e8f670b7036dd34aa99301c9ad641099e403c
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-10-17 08:12:26 +0000

    done

---
+++ basics_2/1_array.js
@@ -0,0 +1,35 @@
+// const Arr =[0,1,2,3,4,5,true,"sarthak"]
+
+const Arr1 =[1,2,3,4,5]
+
+// const arr1 = new Array(1,2,3,4,5)
+
+// Shallow copy (same refernece) & deep copy (diff reference)
+
+
+// console.log(Arr1);
+
+// Arr1.unshift(2) //add element at start 
+// console.log(Arr1);
+
+// Arr1.shift(3) //remove element at start 
+// console.log(Arr1);
+// console.log(typeof Arr1.join()); //make it string
+
+// splice ❤️
+//[same working as slice to print a portion of array]
+// [change the original array ]
+// [as given range element dissapear from original array]
+// [bot range include]
+ 
+// slice ❤️
+// [not chnge on orignal array]
+// [start range include]
+
+
+
+
+
+
+
+
