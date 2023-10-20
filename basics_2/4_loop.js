238cbcf40ea529a28109c5866e0b9419243a27b0
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-10-20 11:13:56 +0000

    done

---
+++ basics_2/4_loop.js
@@ -0,0 +1,76 @@
+// var index =200;
+// for (var index = 0; index < 10; index++) {
+//     // const element = array[index];
+//     console.log(index);    
+// }
+// // index =200;
+// console.log(index);
+
+// let i=0;
+// while (i<5) {
+//     console.log(i);
+//     i++;
+// }
+
+// ❤️ direct loop  ==>high order loop
+
+// let arr =[1,2,3,4,5]
+
+//🙅‍♂️for of loop
+
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+//🙅‍♂️for in loop
+
+// let arr =[10,22,53,64,85]
+//  for (const key in arr) {
+// //    console.log(key); //print the key of array >> key in array are indexes
+//     // console.log(arr[key]); // print element of array
+// }
+
+//🙅‍♂️for each loop => high order function
+
+// let arr =[10,22,53,64,85]
+
+// arr.forEach(function (i){
+//     console.log(i);
+// });
+
+// arr.forEach( i => {
+//     console.log(i);
+// });
+
+// arr.forEach( (element,index,array) => {
+//         console.log(element,index,array);
+// });
+
+// function print(i) {
+//     console.log(i);
+// }
+// arr.forEach( print)
+
+// let array =[
+//     {
+//         name :"sartk",
+//         num:9643,
+//         mail:"sar@123"
+//     },
+//     {
+//         name :"ram",
+//         num:5463,
+//         mail:"ram@123"
+//     },
+//     {
+//         name :"yath",
+//         num:1234,
+//         mail:"yath@123"
+//     }
+// ]
+// array.forEach( i => {
+//     console.log(i["1"]);
+// });
+
+
+
