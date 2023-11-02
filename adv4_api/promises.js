691907361775dfe50b1cb733407db3ca9023ec53
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-11-02 13:59:00 +0000

    dd

--- adv4_api/promises.js
+++ adv4_api/promises.js
@@ -1,12 +1,44 @@
-const promiseOne = new Promise(function (resolve,reject) {
-        // Do an async task
-        //DB calls , cryptography ,network calls
+// const promiseOne = new Promise(function (resolve,reject) {
+//         // Do an async task
+//         //DB calls , cryptography ,network calls
 
-        setTimeout(function(){
-            console.log('Async task complete');
-        },1000)
-});
+//         setTimeout(function(){
+//             console.log('Async task complete');
+//             resolve(); //it triggered .then
+//         },2000)
+// });
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
+// promiseOne.then(function(){
+//     console.log("Promise consumed"); //only work when resolve() call;
+// })
+
+// const promiseTwo = new Promise(function (resolve,reject) {   
+//     setTimeout(function(){
+//         console.log('Async task 2 ');
+//         resolve(); 
+//     },2000)
+// }).then(()=>{
+//     console.log("Async 2 resolver");
+// })
+
+// const promiseThree = new Promise(function (resolve,reject) {   
+//     setTimeout(function(){
+//         resolve({username:"chai",email:"chai@email.com"}); 
+//     },2000)
+// })
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
+
+const promiseFour = new Promise(function (resolve,reject) {   
+    setTimeout(function(){
+        let error =true
+        if(!error){
+        resolve({username:"chai",email:"chai@email.com"}); 
+        }
+    },2000)
 })
+promiseFour.then(function(user){
+    console.log(user);
+})
+
